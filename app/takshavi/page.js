b3a54aa212dac0587b7d0448fd83ac198efbe83d
jalal1985khan <jalal1985khan@gmail.com>
2024-05-02 15:52:53 +0530

    first commit

---
+++ app/takshavi/page.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Banner from '../../components/Takshavi/Banner'
+import Content from '../../components/Takshavi/Content'
+
+
+function AboutUs() {
+  return (
+    <div>
+          <Banner />
+          <Content/>
+
+    </div>
+  )
+}
+
+export default AboutUs
