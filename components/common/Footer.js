ce7f090c165c585de1c0eff71904927fc56c6468
Bhavankumarg <gbhavanbkr@gmail.com>
2025-11-24 15:26:48 +0530

    header, footer links updation

--- components/common/Footer.js
+++ components/common/Footer.js
@@ -28,14 +28,13 @@ function Footer() {
         { href: "/takshavi", label: "Takshavi" },
       ],
     },
-    
     {
       title: "Projects in Goa",
       links: [
         // { label: "Goa - Dona Paula" },
-        { href: "/about-dona-paula", label: "Dona Paula" },
+        // { href: "/about-dona-paula", label: "Dona Paula" },
         { href: "/vista-do-mar", label: "Vista Do Mar" },
-        { href: "/about-moira", label: "Moira" },
+        // { href: "/about-moira", label: "Moira" },
         { href: "/about-amora", label: "Amora" },
         { href: "/about-lucilia", label: "Lucilia" },
       ],
